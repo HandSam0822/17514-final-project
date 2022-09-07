a1cdf5f0f9f86ab8b8399ffaa2e524f55a1365c9
Liaoshousan <shousanl@andrew.cmu.edu>
2022-09-06 23:11:17 -0400

    remove django secret key

--- backend/webapps/settings.py
+++ backend/webapps/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-2b_)&vs6d*p*8is3=@d*5-olpiyei3g7+uvv4*9ab1q4up*lc4"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
